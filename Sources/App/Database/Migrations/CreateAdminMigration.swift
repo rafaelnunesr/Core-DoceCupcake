00ba0b38caa7faecec0253c17abd0d1592cc6ebc
Rafael Rios <rafaelrafssk@hotmail.com>
2024-09-02 19:09:39 -0300

    add enum for admin fields

--- Sources/App/Database/Migrations/CreateAdminMigration.swift
+++ Sources/App/Database/Migrations/CreateAdminMigration.swift
@@ -1,15 +1,15 @@
 import Fluent
 
 struct CreateAdminMigration: AsyncMigration {
-    private let databaseName = "admin"
+    private let databaseName = AdminDbField.schema.rawValue
 
     func prepare(on database: Database) async throws {
         try await database.schema(databaseName)
             .id()
-            .field("created_at", .datetime)
-            .field("user_name", .string, .required)
-            .field("email", .string, .required)
-            .field("password", .string, .required)
+            .field(AdminDbField.createdAt.fieldKey, .datetime)
+            .field(AdminDbField.userName.fieldKey, .string, .required)
+            .field(AdminDbField.email.fieldKey, .string, .required)
+            .field(AdminDbField.password.fieldKey, .string, .required)
             .create()
     }
 
@@ -17,3 +17,22 @@ struct CreateAdminMigration: AsyncMigration {
         try await database.schema(databaseName).delete()
     }
 }
+
+//
+//struct CreateAdminMigration: AsyncMigration {
+//    private let databaseName = AdminDatabaseFields.schema.rawValue
+//
+//    func prepare(on database: Database) async throws {
+//        try await database.schema(databaseName)
+//            .id()
+//            .field("created_at", .datetime)
+//            .field("user_name", .string, .required)
+//            .field("email", .string, .required)
+//            .field("password", .string, .required)
+//            .create()
+//    }
+//
+//    func revert(on database: Database) async throws {
+//        try await database.schema(databaseName).delete()
+//    }
+//}
--- Sources/App/Features/SignUp/Models/Internal/Admin.swift
+++ Sources/App/Features/SignUp/Models/Internal/Admin.swift
@@ -1,22 +1,34 @@
 import Fluent
 import Vapor
 
+enum AdminDbField: String {
+    case schema = "admin"
+    case createdAt = "created_at"
+    case userName = "user_name"
+    case email
+    case password
+    
+    var fieldKey: FieldKey {
+        return FieldKey(stringLiteral: self.rawValue)
+    }
+}
+
 final class Admin: Model {
-    static let schema = "admin"
+    static let schema = AdminDbField.schema.rawValue
 
     @ID(key: .id)
     var id: UUID?
 
-    @Timestamp(key: "created_at", on: .create)
+    @Timestamp(key: AdminDbField.createdAt.fieldKey, on: .create)
     var createdAt: Date?
 
-    @Field(key: "user_name")
+    @Field(key: AdminDbField.userName.fieldKey)
     var userName: String
 
-    @Field(key: "email")
+    @Field(key: AdminDbField.email.fieldKey)
     var email: String
 
-    @Field(key: "password")
+    @Field(key: AdminDbField.password.fieldKey)
     var password: String
 
     internal init() {}
@@ -39,3 +51,45 @@ extension Admin {
         self.init(userName: manager.userName, email: manager.email, password: manager.password)
     }
 }
+
+
+
+//
+//final class Admin: Model {
+//    static let schema = "admin"
+//
+//    @ID(key: .id)
+//    var id: UUID?
+//
+//    @Timestamp(key: "created_at", on: .create)
+//    var createdAt: Date?
+//
+//    @Field(key: "user_name")
+//    var userName: String
+//
+//    @Field(key: "email")
+//    var email: String
+//
+//    @Field(key: "password")
+//    var password: String
+//
+//    internal init() {}
+//
+//    init(id: UUID? = nil,
+//         createdAt: Date? = nil,
+//         userName: String,
+//         email: String,
+//         password: String) {
+//        self.id = id
+//        self.createdAt = createdAt
+//        self.userName = userName
+//        self.email = email
+//        self.password = password
+//    }
+//}
+//
+//extension Admin {
+//    convenience init(from manager: SignUpAdminRequest) {
+//        self.init(userName: manager.userName, email: manager.email, password: manager.password)
+//    }
+//}
